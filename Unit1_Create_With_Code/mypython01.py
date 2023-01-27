9b992defe0862ddebdf58505ba3d9583a971268d
asgibsonmac2023 <24894@stpauls.qld.edu.au>
2023-01-27 12:45:14 +1000

    .

--- Unit1_Create_With_Code/mypython01.py
+++ Unit1_Create_With_Code/mypython01.py
@@ -5,12 +5,14 @@ a mutil line
 comment
 """
 # Input
-name = input ("what is your name? ") 
-age = input ("what is your age? ")
+name = input("what is your name? ")
+age = int(input("what is your age? "))
 
 # Prossesing
-#Python can calculate numbers
-age = 20 + age
+# Python can calculate numbers
+age= str(20 + age)
 
 #output
-print("Hi, " +name "your age in 20 years is", +age".")
+print ("Hi, " +name+ " your age in 20 years is ", age+ ". ")
+print(type(age))
+print(type(name))
