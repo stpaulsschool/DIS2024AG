7385b6a5dcb08df1a17cb0ada232eca8d66231b9
Agibsonsps <123520097+Agibsonsps@users.noreply.github.com>
2023-01-27 12:18:12 +1000

    Pythonage

---
+++ Unit1_Create_With_Code/mypython01.py
@@ -0,0 +1,16 @@
+# This is a comment
+"""
+This is 
+a mutil line
+comment
+"""
+# Input
+name = input ("what is your name? ") 
+age = input ("what is your age? ")
+
+# Prossesing
+#Python can calculate numbers
+age = 20 + age
+
+#output
+print("Hi, " +name "your age in 20 years is", +age".")
