f0cf7513ac0a43424e15d47cbd461e05f465ebd6
asgibsonmac2023 <24894@stpauls.qld.edu.au>
2023-02-09 11:29:28 +1000

    .

--- Unit1_Create_With_Code/How to read from text file/How to read from a text file.py
+++ Unit1_Create_With_Code/How to read from text file/How to read from a text file.py
@@ -1,2 +1,17 @@
-with open("mytextfile.txt", "r") as file:
-    print(file.read())
+with open("mytextfile.txt", "r") as file: #open file from read - "r" access
+    print(file.read()) # read file
+
+mylist = ["a", "b", "c"]
+
+for item in mylist: #for item in list
+    with open("mytextfile.txt", "a") as file: # open the file with access
+        file.write(item) # write each item in list
+        file.write("\n") # write a new line
+
+with open("mytextfile.txt", "r") as file: #open file from read - "r" access
+    print(file.read()) # read file
+
+for item in mylist: #for item in list
+    with open("mytextfile.txt", "w") as file: # open the file with access
+        file.write(" ") # write each item in list
+        file.write("\n") # write a new line
