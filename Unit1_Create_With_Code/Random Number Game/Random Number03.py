5dc763fcac574023271d9b36ef4b9fdac2e6ecdd
asgibsonmac2023 <24894@stpauls.qld.edu.au>
2023-02-10 12:30:21 +1000

    .

--- Unit1_Create_With_Code/Random Number Game/Random Number03.py
+++ Unit1_Create_With_Code/Random Number Game/Random Number03.py
@@ -1,21 +1,25 @@
 import random
 play = input("Would you like to play? Y/N: ").upper()
-guesses = 0
 while play == "Y":
     num = random.randint(0, 100)
     print(num)
     # selecting difficulty with difi as variable
-    difi = int(input("what difficulty would you like? 1, 2 or 3.  (3 being the hardest): "))
-    match difi:
-        case 1:
-            guess_limit = 7
-            guesses = 0
-        case 2:
-            guess_limit = 6
-            guesses = 0
-        case 3:
-            guess_limit = 5
-            guesses = 0
+    guesses = None
+    guess_limit = None
+
+    while not guesses:
+        difi = int(input("what difficulty would you like? 1, 2 or 3.  (3 being the hardest): "))
+        match difi:
+            case 1:
+                guess_limit = 7
+                guesses = 1
+            case 2:
+                guess_limit = 6
+                guesses = 1
+            case 3:
+                guess_limit = 5
+                guesses = 1
+    guesses = guesses - 1
     while guesses < guess_limit:
         print(f"You have {guess_limit - guesses} tries left")
         # printing the amount of tries left every time the user is about to guess
@@ -32,6 +36,6 @@ while play == "Y":
             guesses = guesses + 1
             print("Go lower")
     if guesses >= guess_limit:
-        print(f"Oh no, you hit the guess limit! you lost :(  The number was {num}. You were only {abs(num - guess)} off!")
+        print(f"Noo, you hit the guess limit! you lost :( The number was {num}. You were only {abs(num - guess)} off!")
     play = input("Would you like to play again? Y/N: ").upper()
 print("Thankyou for playing my game.")
