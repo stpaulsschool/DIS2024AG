1217ed835bc209ec3bab2f4b9dd44d6cd3fef8a1
asgibsonmac2023 <24894@stpauls.qld.edu.au>
2023-02-17 14:20:25 +1000

    .

--- Unit1_Create_With_Code/Flask/01registeration/app.py
+++ Unit1_Create_With_Code/Flask/01registeration/app.py
@@ -1,17 +1,20 @@
 # app.py
 from flask import Flask, render_template, request
-#from registration import Registration
+from registration import Registration
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a very long string'
-@approute('/')
+
+
+@app.route('/')
 def hello_form():
     return "<H1>Hello, Form!</H1>"
 
-@app.route('/registration', methods = ['GET', 'POST'])
-def RegisterUser():
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registerUser():
     form = Registration()
     if request.method == 'POST':
-        if form.validate() = False:
+        if form.validate() == False:
             return render_template('registration.html', form=form)
         else:
             return render_template('success.html')
--- Unit1_Create_With_Code/Flask/01registeration/registration.py
+++ Unit1_Create_With_Code/Flask/01registeration/registration.py
@@ -1,14 +1,15 @@
-#registration.py
+# registration.py
 import wtforms as wtforms
 from click import confirm
 from flask_wtf import Form
 # noinspection Py UnresolvedReferences
-form wtforms import StringFeild, PasswordFeild, SubmitFeild, validators, Validationerror
-from wtforms.Validators import InputRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, validators, ValidationError
+from wtforms.validators import InputRequired, Email, EqualTo
 from testemail import IsUnique
 
+
 class Registration(Form):
-    email = StringField("email", [validators.InputRequired ("Please enter your email"),  validators.Email('invalid email'), IsUnique])
-    password = PasswordField('password',[validators.InputRequired ("please enter a password")])
-    confirm_password = PasswordField("confirm password",[validators.InputRequired ("password"), validators.EqualTo("password",  "passwords must match")])
+    email = StringField("email", [validators.InputRequired("Please enter your email"),  validators.Email('invalid email'), IsUnique])
+    password = PasswordField('password', [validators.InputRequired("please enter a password")])
+    confirm_password = PasswordField("confirm password", [validators.InputRequired("password"), validators.EqualTo("password", "passwords must match")])
     submit = SubmitField("Submit")
