e05021753e814c1eb1544238a48b8d0d236a90f9
asgibsonmac2023 <24894@stpauls.qld.edu.au>
2023-02-20 12:34:04 +1000

    .

--- Unit1_Create_With_Code/Flask/01registeration/registration.py
+++ Unit1_Create_With_Code/Flask/01registeration/registration.py
@@ -12,4 +12,4 @@ class Registration(Form):
     email = StringField("email", [validators.InputRequired("Please enter your email"),  validators.Email('invalid email'), IsUnique])
     password = PasswordField('password', [validators.InputRequired("please enter a password")])
     confirm_password = PasswordField("confirm password", [validators.InputRequired("password"), validators.EqualTo("password", "passwords must match")])
-    submit = SubmitField("Submit")
+    submit = SubmitField("submit")
