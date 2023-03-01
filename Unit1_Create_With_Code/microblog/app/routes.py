14124c437ad5986f62ff7605c77ca2db134f89bb
asgibsonmac2023 <24894@stpauls.qld.edu.au>
2023-03-01 13:47:27 +1000

    .

--- Unit1_Create_With_Code/microblog/app/routes.py
+++ Unit1_Create_With_Code/microblog/app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 
 from Unit1_Create_With_Code.microblog.app.forms import LoginForm
 
@@ -18,10 +18,14 @@ def index():
             'body': 'Basketball is the best sport!'
         }
     ]
-    return render_template('index.html',title=user['username'], user=user, posts=posts)
+    return render_template('index.html', title=user['username'], user=user, posts=posts)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET' 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
-
