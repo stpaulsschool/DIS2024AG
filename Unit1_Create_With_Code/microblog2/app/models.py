7a150f1bc65d55018cbd3a08e9be5eddecee3d42
asgibsonmac2023 <24894@stpauls.qld.edu.au>
2023-03-06 12:17:04 +1000

    .

---
+++ Unit1_Create_With_Code/microblog2/app/models.py
@@ -0,0 +1,12 @@
+from Unit1_Create_With_Code.microblog2.app import db
+
+
+class User(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), idex=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+
+def __repr__(self):
+    return '<User {}>'.format(self.username)
